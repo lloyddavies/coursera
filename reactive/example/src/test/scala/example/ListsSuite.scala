fd7bc9fff6043ebe672fc67f381f0d10c5d15fdf
Lloyd Davies <lloyd@naramedia.com>
2015-04-16 11:58:33 +0100

    Completed Week 0

--- reactive/example/src/test/scala/example/ListsSuite.scala
+++ reactive/example/src/test/scala/example/ListsSuite.scala
@@ -45,7 +45,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
 
   /**
@@ -69,7 +69,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 !== 3) // Fix me, please!
   }
 
   /**
@@ -112,9 +112,15 @@ class ListsSuite extends FunSuite {
    */
   test("sum of a few numbers") {
     assert(sum(List(1,2,0)) === 3)
+    assert(sum(List()) === 0)
+    assert(sum(List(-2, 1)) === -1)
   }
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+    assert(max(List(1, -2)) === 1)
+    intercept[NoSuchElementException] {
+      max(List())
+    }
   }
 
 }
