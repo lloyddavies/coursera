2cfb4f4eae2e47b529e17cb4a290b3161fc3c24a
Lloyd Davies <lloyd@naramedia.com>
2015-05-11 21:02:52 +0100

    Week 4 complete

--- reactive/suggestions/src/main/scala/suggestions/gui/WikipediaSuggest.scala
+++ reactive/suggestions/src/main/scala/suggestions/gui/WikipediaSuggest.scala
@@ -81,25 +81,28 @@ object WikipediaSuggest extends SimpleSwingApplication with ConcreteSwingApi wit
      */
 
     // TO IMPLEMENT
-    val searchTerms: Observable[String] = ???
+    val searchTerms: Observable[String] = searchTermField.textValues
 
     // TO IMPLEMENT
-    val suggestions: Observable[Try[List[String]]] = ???
+    val suggestions: Observable[Try[List[String]]] = searchTerms.concatRecovered(wikiSuggestResponseStream).timedOut(1)
 
     // TO IMPLEMENT
     val suggestionSubscription: Subscription =  suggestions.observeOn(eventScheduler) subscribe {
-      x => ???
+      _ match {
+          case Success(xs) => suggestionList.listData = xs
+          case Failure(e) => status.text = e.getMessage
+        }
     }
 
     // TO IMPLEMENT
-    val selections: Observable[String] = ???
+    val selections: Observable[String] = button.clicks.map(_ => suggestionList.listData.head)
 
     // TO IMPLEMENT
-    val pages: Observable[Try[String]] = ???
+    val pages: Observable[Try[String]] = selections.flatMap(wikiPageResponseStream(_)).recovered
 
     // TO IMPLEMENT
     val pageSubscription: Subscription = pages.observeOn(eventScheduler) subscribe {
-      x => ???
+      x => editorpane.text = x.getOrElse("Error!")
     }
 
   }
